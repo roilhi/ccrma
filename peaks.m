dd90ef001bc1c32eaa524ddf8a67917c2b6cfc93
Leigh Smith <leighs@ccrma-gate.stanford.edu>
2013-06-24 16:09:37 -0700

    Replaced Octave only code with Matlab versions

--- peaks.m
+++ peaks.m
@@ -1,10 +1,10 @@
 % -*- octave -*-
 function peaks = peaks(signal,minimum)
-  signalPlus = shift(signal, 1);
-  signalMinus = shift(signal, -1);
+  signalPlus = circshift(signal, [1, 1]);
+  signalMinus = circshift(signal, [1, -1]);
   if nargin < 2
     peaks = signal > signalPlus & signal > signalMinus;
   else
     peaks = signal < signalPlus & signal < signalMinus;
   end
-endfunction
+end
