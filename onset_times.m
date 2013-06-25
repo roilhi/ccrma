0adb56d483519fc6fa9be99de76b05f1801244b1
Leigh Smith <lsmith@izotope.com>
2013-06-25 14:53:55 -0700

    Now compensates for the spectrum filter delay

--- onset_times.m
+++ onset_times.m
@@ -1,8 +1,8 @@
-function [onsets_seconds, attack_times] = onset_times(audio_signal, sample_rate)
+function [onsets_seconds, attack_durations] = onset_times(audio_signal, sample_rate)
 % Return times in seconds of the location of onsets in the audio file.
   threshold = 1.5;
 
-  [wodf, odf_sr] = odf_of_signal(audio_signal, sample_rate);
+  [wodf, odf_sr, subband_odfs, ODF_start_seconds] = odf_of_signal(audio_signal, sample_rate);
   % Find the relative difference of the wide-band onset detection function. 
   % This identifies rapid attack slopes.
   wodf_diff = diff(wodf);
@@ -11,13 +11,18 @@ function [onsets_seconds, attack_times] = onset_times(audio_signal, sample_rate)
 
   bigPeaks = (wodf_diff .* allPeaks) > (std(wodf_diff) * threshold);
   peak_times = find(bigPeaks);
-  % All values will be moved forward one because of the 1st order difference.
-  % Also, we've found the peaks of the relative difference, however, what we want are the
+
+  % We've found the peaks of the relative difference, however, what we ultimately want are the
   % attack onset times preceding these. Therefore the compensation descends from the peaks to
   % the nearest preceding troughs.
   onset_times = find_onset_attacks(wodf, peak_times, odf_sr);
-  attack_times = (peak_times - onset_times) ./ odf_sr;
+  attack_durations = (peak_times - onset_times) ./ odf_sr;
 
-  onsets_seconds = onset_times ./ odf_sr;
+  % figure()
+  % plot(wodf, "1", onset_times, wodf(onset_times), "2^")
+  % figure()
+  % plot(odf_downsampled_signal * 4, "1", wodf, "2")
+
+  % Compensate for the ODF 
+  onsets_seconds = (onset_times ./ odf_sr) - ODF_start_seconds;
 end
-
