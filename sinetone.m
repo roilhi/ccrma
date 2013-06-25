f8e451da704e2132ceba50a004c1e02818b04423
Leigh Smith <leighs@cmn7.stanford.edu>
2013-06-24 20:58:23 -0700

    Changes to work on Matlab as well as Octave

--- sinetone.m
+++ sinetone.m
@@ -3,10 +3,10 @@ function sinetone = sinetone(frequency, sampleRate, duration, amplitude, phase)
 % frequency in Hz, bipolar normalized amplitude, duration in seconds.
   if(nargin < 4)
     sampleRate = 44100;
-  endif
+  end
   if(nargin < 5)
     phase = 0; % radians
-  endif
+  end
   radianFreq = ([0 : 1 / sampleRate : duration - 1/sampleRate] .* 2 .* pi * frequency);
   sinetone = amplitude * sin(radianFreq + phase); 
 end
--- test_onsets.m
+++ test_onsets.m
@@ -9,11 +9,14 @@ function marked_onset_times = test_onsets(inputSoundPath)
   [marked_onset_times, attack_times] = onset_times(mono_audio_signal, sample_rate);
 
   figure()
-  % plot(mono_audio_signal, "1")
-  plot(mono_audio_signal, "1", marked_onset_times .* sample_rate, ones(1,length(marked_onset_times)), "2^")
+  % plot(mono_audio_signal, '1')
+  % plot(mono_audio_signal, '1', marked_onset_times .* sample_rate, ones(1,length(marked_onset_times)), '2^')
+  plot(1:length(mono_audio_signal), mono_audio_signal, '-', marked_onset_times .* sample_rate, ones(1,length(marked_onset_times)), '^')
 
+  
   beep = sinetone(880, sample_rate, 0.050, 0.8);
-  % wavwrite(beep, sample_rate, "/tmp/beep.wav");
+  % wavwrite(beep, sample_rate, '/tmp/beep.wav');
   [dir, filename, ext] = fileparts(inputSoundPath);
-  save_rhythm_mix([dir, "/", filename, "_onsets.wav"], inputSoundPath, marked_onset_times, beep);
+  fileToSave = fullfile(dir, [filename, '_onsets.wav']);
+  save_rhythm_mix(fileToSave, inputSoundPath, marked_onset_times, beep);
 end
